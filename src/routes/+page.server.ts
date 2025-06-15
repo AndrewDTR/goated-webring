1dac98d2b6f0bf46ffa6649c2dd06147ea8e4c19
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-06-15 01:55:10 -0700

    base index list page

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,124 @@
+import type { PageServerLoad } from './$types';
+import { sites } from '$lib/server/db/schema';
+import { drizzle } from 'drizzle-orm/libsql';
+import { DATABASE_URL } from '$env/static/private';
+
+const db = drizzle(DATABASE_URL);
+
+const animalEmojis = [
+	// Mammals
+	'🐒',
+	'🦍',
+	'🦧',
+	'🐕',
+	'🦮',
+	'🐕‍🦺',
+	'🐩',
+	'🐺',
+	'🦊',
+	'🦝',
+	'🐈',
+	'🦁',
+	'🐅',
+	'🐆',
+	'🐴',
+	'🐎',
+	'🦓',
+	'🦌',
+	'🐂',
+	'🐃',
+	'🐄',
+	'🐖',
+	'🐗',
+	'🐏',
+	'🐑',
+	'🐐',
+	'🐪',
+	'🐫',
+	'🦙',
+	'🦒',
+	'🐘',
+	'🦏',
+	'🦛',
+	'🐁',
+	'🐀',
+	'🐹',
+	'🐇',
+	'🐿️',
+	'🦔',
+	'🦇',
+	'🐻',
+	'🐨',
+	'🐼',
+	'🦥',
+	'🦦',
+	'🦨',
+	'🦘',
+	'🦡',
+
+	// Birds
+	'🦃',
+	'🐔',
+	'🐓',
+	'🐣',
+	'🐤',
+	'🐥',
+	'🐦',
+	'🐧',
+	'🕊️',
+	'🦅',
+	'🦆',
+	'🦢',
+	'🦉',
+	'🦩',
+	'🦜',
+
+	// Marine & Reptiles
+	'🐸',
+	'🐊',
+	'🐢',
+	'🦎',
+	'🐍',
+	'🐳',
+	'🐋',
+	'🐬',
+	'🐟',
+	'🐠',
+	'🐡',
+	'🦈',
+	'🐙',
+	'🦀',
+	'🦞',
+	'🦑',
+
+	// Bugs
+	'🐌',
+	'🦋',
+	'🐛',
+	'🐜',
+	'🐝',
+	'🐞'
+];
+
+// https://stackoverflow.com/a/19270021
+function getRandom(arr: string[], n: number) {
+	var result = new Array(n),
+		len = arr.length,
+		taken = new Array(len);
+	if (n > len) throw new RangeError('getRandom: more elements taken than available');
+	while (n--) {
+		var x = Math.floor(Math.random() * len);
+		result[n] = arr[x in taken ? taken[x] : x];
+		taken[x] = --len in taken ? taken[len] : len;
+	}
+	return result;
+}
+
+export const load: PageServerLoad = async () => {
+	const siteList = await db.select({ site: sites.link }).from(sites).orderBy(sites.order);
+
+	return {
+		sites: siteList,
+		emojis: getRandom(animalEmojis, siteList.length)
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1 class="text-3xl m-2 bg-red-300">you like kissing boys dont you</h1>
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>goated webring</title>
+</svelte:head>
+
+<h1>🐐 goated webring</h1>
+
+{#each data.sites as site, index}
+	<div class="outline-4">
+		<a href={site.site}>{data.emojis[index]} {site.site}</a>
+	</div>
+{/each}
