33d5307e45113881d3523d441dd874a54c265299
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-08-26 03:05:23 -0400

    test

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,11 @@
 import { drizzle } from 'drizzle-orm/libsql';
+import { execSync } from 'node:child_process';
 import { env } from '$env/dynamic/private';
 import { settings } from '$lib/server/db/schema';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+await execSync('npm run db:push');
 
 export const db = drizzle(env.DATABASE_URL);
 
