0608f3e15fa07c07332fb3376b6403fb9ab7e09f
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-08-26 17:44:57 -0400

    remove adding site test page

--- src/routes/test/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import addSite from '$lib/addSite';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	await addSite(`https://${Math.random().toString(36).slice(2)}.com`);
-	return {};
-};
